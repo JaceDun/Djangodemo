5a770930de52ae28566535f9b4fbae0fd638e9cb
Administrator <609694785@qq.com>
2019-11-01 11:11:38 +0800

    modifed something

--- mydemo/mydemo/urls.py
+++ mydemo/mydemo/urls.py
@@ -4,9 +4,11 @@ from . import users_manage, testdb, search, register, logion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', users_manage.list_users),
+    #path('index/', users_manage.list_users),
     path('list-emp/', testdb.query_emp),
     path('search/', search.search_post),
     path('register/', register.register_user),
-    path('logion/', logion.logion_check),
+    path('check/', logion.index),
+    path('login/', logion.logion_check),
+
 ]
