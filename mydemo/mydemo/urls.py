efca4cbe54e0c52dbd8728f64c5ea3826ac65a6b
Administrator <609694785@qq.com>
2019-11-06 16:56:20 +0800

    update mydemo20191106

--- mydemo/mydemo/urls.py
+++ mydemo/mydemo/urls.py
@@ -4,9 +4,9 @@ from . import login, user_manage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('add_user/', user_manage.add_user),
-    path('index/', login.login_index),
     path('login/', login.login_check),
+    path('index/', login.login_index),
+    path('add_user/', user_manage.add_user),
     path('edit_user/', user_manage.edit_user),
     path('edit_save/', user_manage.edit_user_handler),
     path('delete_user/', user_manage.delete_user),
